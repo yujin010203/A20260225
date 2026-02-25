f5f6a619a209aa5b9987e20a610cdc0d47f10d20
yujin010203 <yyuyu02@naver.com>
2026-02-25 09:02:49 +0900

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int Number1;
+	int Number2;
+	cin >> Number1;
+	cin >> Number2;
+	cout << Number1 + Number2 + 3 * Number1 - Number2 << endl;
+
+	return 0;
+}
